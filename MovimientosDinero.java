8995616ae014391b9a5e5ee0db498614e9b47330
Danileon001 <94156654+Danileon001@users.noreply.github.com>
2022-08-27 14:24:15 -0500

    Se Modela la clase MovimienovimientoDinero

---
+++ MovimientosDinero.java
@@ -0,0 +1,38 @@
+package mintic;
+
+public class MovimientosDienro {
+
+    private Long Monto;
+    private String Concepto;
+    private String Usuario;
+
+    public MovimientosDienro(Long monto, String concepto, String usuario) {
+        Monto = monto;
+        Concepto = concepto;
+        Usuario = usuario;
+    }
+
+    public Long getMonto() {
+        return Monto;
+    }
+
+    public void setMonto(Long monto) {
+        Monto = monto;
+    }
+
+    public String getConcepto() {
+        return Concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        Concepto = concepto;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        Usuario = usuario;
+    }
+}
