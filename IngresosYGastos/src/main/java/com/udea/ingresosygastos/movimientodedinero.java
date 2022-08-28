aec50fd01dceaeffd614223639efa9803c62a9a3
Andres Mejia <eamejia783@gmail.com>
2022-08-28 00:19:21 -0500

    Update movimientodedinero.java

--- IngresosYGastos/src/main/java/com/udea/ingresosygastos/movimientodedinero.java
+++ IngresosYGastos/src/main/java/com/udea/ingresosygastos/movimientodedinero.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.udea.ingresosygastos;
 
-/**
- *
- * @author voyag
- */
+
 public class movimientodedinero {
-    
+    private Long monto;
+    private String concepto;
+    private String usuario;
+
+    public movimientodedinero(Long monto, String concepto, String usuario) {
+        this.monto = monto;
+        this.concepto = concepto;
+        this.usuario = usuario;
+    }
+
+    public Long getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Long monto) {
+        this.monto = monto;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+     
+    @Override
+    public String toString() {
+        return "Movimiento Dinero{" +
+                "Monto='" + monto + '\'' +
+                ", Concepto='" + concepto + '\'' +
+                ", Usuario=" + usuario +
+                '}';
+    }
 }
