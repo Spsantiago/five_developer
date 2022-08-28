7b4423c7950463f8f9965ab559811d020fa077b8
Andres Mejia <eamejia783@gmail.com>
2022-08-28 01:00:01 -0500

    Update IngresosYGastos.java

--- IngresosYGastos/src/main/java/com/udea/ingresosygastos/IngresosYGastos.java
+++ IngresosYGastos/src/main/java/com/udea/ingresosygastos/IngresosYGastos.java
@@ -5,11 +5,11 @@ package com.udea.ingresosygastos;
 public class IngresosYGastos {
 
     public static void main(String[] args) {
+             
+        empresa emp = new empresa("Grupo Bysa S.A.S", "Cra 1 # 1-115", "6027735505", "901518345-2");
         
-       empresa empresa1 = new empresa("Grupo Bysa S.A.S", "Cra 1 # 1-115", "6027735505", "901518345-2");
-       
-        System.out.println(empresa1);
-       
+        System.out.println(emp.getDireccion());
+               
        
     }
 }
