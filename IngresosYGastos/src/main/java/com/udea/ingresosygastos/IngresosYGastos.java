d91ede0188a95e55ee797416a28703da0f4ed22d
Spsantiago <spsantiago0410@gmail.com>
2022-09-08 23:44:17 -0500

    Update IngresosYGastos.java

--- IngresosYGastos/src/main/java/com/udea/ingresosygastos/IngresosYGastos.java
+++ IngresosYGastos/src/main/java/com/udea/ingresosygastos/IngresosYGastos.java
@@ -7,8 +7,9 @@ public class IngresosYGastos {
     public static void main(String[] args) {
              
         empresa emp = new empresa("Grupo Bysa S.A.S", "Cra 1 # 1-115 panamericana", "6027735505", "901518345-2");
-        movimientodedinero movimiento = new movimientodedinero(10500, "gasto", "eamejia");
-        empleado empleado = new empleado("juan", "perez", "juanperez@gmai.com","Grupo Bysa S.A.S");
+        empleado empleado = new empleado("juan", "perez", "juanperez@gmai.com",emp);
+        movimientodedinero movimiento = new movimientodedinero(10500, "gasto", empleado);
+        
         System.out.println(emp.toString());
         System.out.println(movimiento.toString());               
         System.out.println(empleado.toString());
