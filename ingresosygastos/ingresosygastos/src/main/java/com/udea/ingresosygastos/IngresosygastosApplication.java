bed17cfdeef30975b862f3642c098d0db0285283
Spsantiago <spsantiago0410@gmail.com>
2022-09-11 22:43:23 -0500

    Create IngresosygastosApplication.java

---
+++ ingresosygastos/ingresosygastos/src/main/java/com/udea/ingresosygastos/IngresosygastosApplication.java
@@ -0,0 +1,13 @@
+package com.udea.ingresosygastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngresosygastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngresosygastosApplication.class, args);
+	}
+
+}
