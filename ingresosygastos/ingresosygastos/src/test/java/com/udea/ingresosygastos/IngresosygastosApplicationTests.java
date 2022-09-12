7d2f7253da050a5e19bd472303077f1531b3ea98
Spsantiago <spsantiago0410@gmail.com>
2022-09-11 22:43:58 -0500

    Create IngresosygastosApplicationTests.java

---
+++ ingresosygastos/ingresosygastos/src/test/java/com/udea/ingresosygastos/IngresosygastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.ingresosygastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngresosygastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
