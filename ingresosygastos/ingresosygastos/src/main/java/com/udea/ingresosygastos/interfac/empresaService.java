4dc149d846c6a976883da7093f8b91d7dc918a12
Spsantiago <spsantiago0410@gmail.com>
2022-09-18 23:11:01 -0500

    Update empresaService.java

--- ingresosygastos/ingresosygastos/src/main/java/com/udea/ingresosygastos/interfac/empresaService.java
+++ ingresosygastos/ingresosygastos/src/main/java/com/udea/ingresosygastos/interfac/empresaService.java
@@ -6,7 +6,7 @@ import com.udea.ingresosygastos.entityes.empresa;
 import java.util.List;
 public interface empresaService {
     List<empresa>listar();
-    empresa listarcorreo(String NIT);
+    empresa listarNIT(String NIT);
     empresa add(empresa empr);
     empresa edit(empresa empr);
     empresa delete(String NIT);
