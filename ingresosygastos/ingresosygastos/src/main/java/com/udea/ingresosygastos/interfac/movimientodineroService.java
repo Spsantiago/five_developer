b95815f9912617fc2dbd5fe38dbaca51059b54f9
Spsantiago <spsantiago0410@gmail.com>
2022-09-18 22:57:03 -0500

    Update movimientodineroService.java

--- ingresosygastos/ingresosygastos/src/main/java/com/udea/ingresosygastos/interfac/movimientodineroService.java
+++ ingresosygastos/ingresosygastos/src/main/java/com/udea/ingresosygastos/interfac/movimientodineroService.java
@@ -4,7 +4,9 @@ import com.udea.ingresosygastos.entityes.movimientodinero;
 import java.util.List;
 public interface movimientodineroService {
     List<movimientodinero>listar();
-    movimientodinero listarcorreo(String concepto);
+
+    movimientodinero listarconcepto(String concepto);
+
     movimientodinero add(movimientodinero movimeinto);
     movimientodinero edit(movimientodinero movimeinto);
     movimientodinero delete(String concepto);
